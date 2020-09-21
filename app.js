8f8cdca08a37e0a4bb26288ab6932d06baa579ca
Michael Verdi <michael.v.verdi@gmail.com>
2020-09-21 15:11:16 -0400

    added additional console.log statements for clarity

--- app.js
+++ app.js
@@ -8,10 +8,16 @@ fs.readFile("input.txt", "utf8", (err, file) => {
   }
 
   let data = file.split("\n");
+  let dupData = data.slice();
 
   try {
     const roomba = new Roomba(data);
+    console.log("Reading file: input.txt");
+    console.log(`Based on the following inputs: ${dupData}`);
+    console.log("Roomba is up and running...");
+
     const result = roomba.run();
+
     console.log(`
       Final Position ------> ${result.finalPosition}
       Total Dirt Cleaned --> ${result.spotsCleaned}`);
